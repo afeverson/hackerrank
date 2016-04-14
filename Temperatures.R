2e887f1416cf9705410e428b1af31c35bd580d2b
afeverson <augustfeverson@gmail.com>
2016-04-14 14:36:12 -0400

    Update Temperatures.R
    Updated to output the correctly formatted answers... I still need to implement accurate models though

--- Temperatures.R
+++ Temperatures.R
@@ -7,10 +7,29 @@ train <- data[grep('Missing', data$tmax, invert = T),]
 train <- train[grep('Missing', train$tmin, invert = T),]
 test_max <- data[grep('Missing', data$tmax),]
 test_min <- data[grep('Missing', data$tmin),]
+train$tmax <- as.numeric(as.character(train$tmax))
+train$tmin <- as.numeric(as.character(train$tmin))
+
+test_max$tmin <- as.numeric(as.character(test_max$tmin))
+test_min$tmax <- as.numeric(as.character(test_min$tmax))
 
 #models built for tmax and tmin 
 max_mod <- lm(tmax~., train)
 min_mod <- lm(tmin~., train)
 
-max_mod
-min_mod
+#applying models to the set with the missing values
+max_pred <- predict(max_mod, test_max[,c(1,2,4)])
+min_pred <- predict(min_mod, test_min[,c(1,2,3)])
+
+fin_max <- data.frame(test_max$tmax)
+fin_max$pred <- max_pred
+colnames(fin_max) <- c('miss','pred')
+fin_min <- data.frame(test_min$tmin)
+fin_min$pred <- min_pred
+colnames(fin_min) <- c('miss','pred')
+
+final <- rbind(fin_max, fin_min)
+
+ans <- final[sort(as.integer(substr(final$miss,9,100))),]
+
+write.table(ans$pred, row.names = F, col.names = F, quote = F)
