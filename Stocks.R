b1766aa113bd31336ea183f3bdec5f7bf57e3f25
afeverson <augustfeverson@gmail.com>
2016-05-16 16:55:31 -0400

    Updating to submit a proper answer... again
    This time the output actually causes actions.  Now I have to make some money.

--- Stocks.R
+++ Stocks.R
@@ -4,13 +4,22 @@ stocks <- read.table('/dev/stdin', sep = ' ', skip = 1, header = F)
 trans <- data.frame()
 for (stock in 1:nrow(stocks)) {
     if (stocks[stock,6] <= stocks[stock,5] && stocks[stock,6] <= stocks[stock,4] && stocks[stock,6] <= stocks[stock,3]) {
-        trans <- rbind(trans,c(as.character(stocks[stock,1]),'BUY',10))
+        if (as.numeric(mkd[1]) >= as.numeric(stocks[stock,7])) {
+            trans <- rbind(trans,c(as.character(stocks[stock,1]),'BUY',as.numeric(mkd[1])%/%as.numeric(stocks[stock,7])))
+            mkd[1] <- mkd[1] - ((as.numeric(mkd[1])%/%as.numeric(stocks[stock,7]))*stocks[stock,7])
+        }
     }
-    if (stocks[stock,6] >= stocks[stock,5] || stocks[stock,6] >= stocks[stock,4] || stocks[stock,6] >= stocks[stock,3]) {
+    if (stocks[stock,6] >= stocks[stock,5] | stocks[stock,6] >= stocks[stock,4] | stocks[stock,6] >= stocks[stock,3]) {
+        if (stocks[stock,2] > 0) {
+            trans <- rbind(trans, c(as.character(stocks[stock,1]),'SELL',stocks[stock,2]))
+        }
+    }
+    if (mkd[3] == 1) {
         if (stocks[stock,2] > 0) {
             trans <- rbind(trans, c(as.character(stocks[stock,1]),'SELL',stocks[stock,2]))
         }
     }
 }
 
+write.table(as.integer(nrow(trans)), row.names = F, col.names = F, quote = F)
 write.table(trans, row.names = F, col.names = F, quote = F)
