e95a5b6444f7d76c6f6ff7e78fbb98b61915ece2
afeverson <augustfeverson@gmail.com>
2016-05-16 17:08:54 -0400

    small stuff

--- Stocks.R
+++ Stocks.R
@@ -2,7 +2,8 @@ mkd <- read.table('/dev/stdin', header = F, nrow = 1)
 stocks <- read.table('/dev/stdin', sep = ' ', skip = 1, header = F)
 
 trans <- data.frame()
-for (stock in 1:nrow(stocks)) {
+
+for (stock in 1:as.integer(mkd[2])) {
     if (stocks[stock,6] <= stocks[stock,5] && stocks[stock,6] <= stocks[stock,4] && stocks[stock,6] <= stocks[stock,3]) {
         if (as.numeric(mkd[1]) >= as.numeric(stocks[stock,7])) {
             trans <- rbind(trans,c(as.character(stocks[stock,1]),'BUY',as.numeric(mkd[1])%/%as.numeric(stocks[stock,7])))
@@ -12,11 +13,13 @@ for (stock in 1:nrow(stocks)) {
     if (stocks[stock,6] >= stocks[stock,5] | stocks[stock,6] >= stocks[stock,4] | stocks[stock,6] >= stocks[stock,3]) {
         if (stocks[stock,2] > 0) {
             trans <- rbind(trans, c(as.character(stocks[stock,1]),'SELL',stocks[stock,2]))
+            mkd[1] <- mkd[1] + (stocks[stock,2]*stocks[stock,7])
         }
     }
     if (mkd[3] == 1) {
         if (stocks[stock,2] > 0) {
             trans <- rbind(trans, c(as.character(stocks[stock,1]),'SELL',stocks[stock,2]))
+            mkd[1] <- mkd[1] + (stocks[stock,2]*stocks[stock,7])
         }
     }
 }
